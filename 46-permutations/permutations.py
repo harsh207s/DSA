8fad8d34ba0499b5eb8569aa071a924cfc807b0e
Harsh Vardhan Singh <165757125+harsh207s@users.noreply.github.com>
2025-08-27 17:52:28 +0530

    Time: 3 ms (31.34%) | Memory: 18 MB (33.89%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(path, remaining):
+            if not remaining:
+                res.append(path[:])   # copy current path
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+
+        backtrack([], nums)
+        return res
