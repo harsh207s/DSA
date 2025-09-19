c19f27468fea157e276d66114aa586b956a6a80e
Harsh Vardhan Singh <165757125+harsh207s@users.noreply.github.com>
2025-09-19 10:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.49%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        pigs = 0
+        while (states ** pigs) < buckets:
+            pigs += 1
+        return pigs
